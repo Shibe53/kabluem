58d360b28f26adbd4b6d62a7e91675d01b2ea7e3
Shibe53 <raulserban05@gmail.com>
2025-08-11 16:50:02 +0300

    Player controller, world +extras
    Added some useful extras for later (hitboxes, hurtboxes)

---
+++ Hitboxes/hurtbox.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+#const HitEffect = preload("res://Effects/hit_effect.tscn")
+
+signal invincibility_started
+signal invincibility_ended
+
+@onready var timer = $Timer
+@onready var collisionShape = $CollisionShape
+
+var invincible = false:
+	set(value):
+		invincible = value
+		if invincible == true:
+			emit_signal("invincibility_started")
+		else:
+			emit_signal("invincibility_ended")
+
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start(duration)
+'''
+func create_hit_effect():
+	var effect = HitEffect.instantiate()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+'''
+func _on_timer_timeout():
+	self.invincible = false
+
+func _on_invincibility_started():
+	collisionShape.set_deferred("disabled", true)
+
+func _on_invincibility_ended():
+	collisionShape.set_deferred("disabled", false)
---
+++ Player/player.gd
@@ -0,0 +1,103 @@
+extends CharacterBody2D
+
+#const PlayerHurtSound = preload("res://Music and Sounds/player_hurt_sound.tscn")
+
+const ACCELERATION = 800
+const MAX_SPEED = 200
+const DODGE_SPEED = 115
+const FRICTION = 500
+
+enum {
+	MOVE,
+	DODGE,
+	THROW
+}
+
+var state = MOVE
+var dodge_vector = Vector2.DOWN
+var stats = PlayerStats
+'''
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+@onready var swordHitbox = $HitboxPivot/SwordHitbox
+@onready var hurtbox = $Hurtbox
+@onready var blinkAnimationPlayer = $BlinkAnimationPlayer
+'''
+func _ready():
+	randomize()
+	stats.no_health.connect(queue_free)
+#	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		DODGE:
+			dodge_state()
+		THROW:
+			throw_state()
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		dodge_vector = input_vector
+#		animationTree.set("parameters/Idle/blend_position", input_vector)
+#		animationTree.set("parameters/Run/blend_position", input_vector)
+#		animationTree.set("parameters/Attack/blend_position", input_vector)
+#		animationTree.set("parameters/Roll/blend_position", input_vector)
+#		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+#		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("dodge"):
+		state = DODGE
+	
+	if Input.is_action_just_pressed("throw"):
+		state = THROW
+
+func throw_state():
+	velocity = Vector2.ZERO
+#	animationState.travel("Attack")
+
+func throw_animation_finished():
+	state = MOVE
+
+func dodge_state():
+	velocity = dodge_vector * DODGE_SPEED
+#	animationState.travel("Roll")
+	move_and_slide()
+#	hurtbox.start_invincibility(0.15)
+
+func dodge_animation_finished():
+	state = MOVE
+	velocity /= 1.5
+
+func _on_hurtbox_area_entered(area):
+	stats.health -= area.damage
+	var knockback_direction = area.owner.position.direction_to(position)
+	velocity = knockback_direction * 200
+#	hurtbox.start_invincibility(0.6)
+#	start_blinking()
+#	hurtbox.create_hit_effect()
+#	var playerHurtSound = PlayerHurtSound.instantiate()
+#	get_tree().current_scene.add_child(playerHurtSound)
+
+func start_blinking():
+#	blinkAnimationPlayer.play("Start")
+	pass
+
+func _on_hurtbox_invincibility_ended():
+#	blinkAnimationPlayer.play("Stop")
+	pass
+
+func _on_hurtbox_invincibility_started():
+	pass
