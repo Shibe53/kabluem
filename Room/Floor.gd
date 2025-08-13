8d5e737834b26fdf43b847401d1a39d9d26bd7e7
Shibe53 <raulserban05@gmail.com>
2025-08-13 20:32:07 +0300

    Added in game GUI and bloom stats

--- Room/Floor.gd
+++ Room/Floor.gd
@@ -1,6 +1,8 @@
 extends TileMapLayer
 class_name Floor
 
+var bloom_tiles = 0
+
 # if its not in any terrain get_cell_tile_data() returns -1
 func bloom(col_shape: CollisionShape2D):
 	if is_instance_of(col_shape.shape, CircleShape2D):
@@ -11,7 +13,10 @@ func bloom(col_shape: CollisionShape2D):
 			if Geometry2D.is_point_in_circle(map_to_local(cell), to_local(col_shape.global_position), float_to_local(circle.radius*col_shape.global_scale.x)):
 				if get_cell_tile_data(cell).terrain != 0:
 					affected_tiles.append(cell)
-					
+					bloom_tiles += 1
+		
+		PlayerStats.bloom += bloom_tiles
+		bloom_tiles = 0
 		set_cells_terrain_connect(affected_tiles, 0, 0, true)
 
 #this only really makes sense if both global_scale.x and global_scale.y are equal but alas
