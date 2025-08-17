72abc1a376854c24d9b2cd58afa1182401a8333a
Lore <lorenyanski@gmail.com>
2025-08-17 13:37:46 +0200

    level 3

--- Levels/level.gd
+++ Levels/level.gd
@@ -2,15 +2,21 @@ extends Node2D
 
 @export var level_number = 1
 @export var bloom_needed = 1
+@export var bloom_percentage_needed = 0
 @export var player_max_health = 3
 
 @onready var gui = $CanvasLayer/Gui
 @onready var endpoint = $Endpoint
+@onready var concrete_layer = $NavigationRegion2D/TileMap/TileMapLayer
 
 var stats = PlayerStats
 var level_select = LevelSelect
 
 func _ready():
+	print(concrete_layer.get_used_cells().size())
+	if bloom_percentage_needed != 0:
+		bloom_needed = bloom_percentage_needed * concrete_layer.get_used_cells().size() / 100
+		print(bloom_needed)
 	level_select.room_bloomed.connect(kill_enemies)
 	level_select.no_enemies_left.connect(go_to_endpoint)
 	level_select.bloom_needed = bloom_needed
@@ -29,6 +35,7 @@ func kill_enemies():
 		gui.change_obj_label("Free the rest of them")
 	else:
 		gui.change_obj_label("Find a spot to burrow")
+		endpoint.canEnd = true
 
 func go_to_endpoint():
 	gui.change_obj_label("Find a spot to burrow")
